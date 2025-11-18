754d530886ee42fb2508edd710e804bb177fd5f6
SHAHIN <shahinhasainarth@shahins-MacBook-Air.local>
2025-11-18 11:01:41 +0530

    day-2

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from "../header/header";
 import { Footer } from "../footer/footer";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Footer],
+  imports: [Header, Footer, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
